fdc20d9791c52459dce410a0f686fd0a5680d111
ONYX <onyx.tm.mb@gmail.com>
2017-01-24 18:32:24 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+header("Content-type: text/html; charset=utf-8");
+global $mysqli;
+$mysqli = new mysqli('localhost', 'id472544_demo','phpbots', 'id472544_demo');
+$mysqli->set_charset("utf8");
+
